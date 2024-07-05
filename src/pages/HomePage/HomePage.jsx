e3aa1071d762fdcabf55447ce093d700b1210158
Makaka449 <145019010+Makaka449@users.noreply.github.com>
2024-07-05 14:18:26 +0300

    up

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import style from './HomePage.module.css';
+
+const HomePage = () => {
+  return (
+    <>
+      <div className={style.container}>
+        <h1 className={style.title}>
+          Phonebook
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
